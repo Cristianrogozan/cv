0a2bfb43f78c38d73de701a0041003e3223e80fc
Cristianrogozan <116027431+Cristianrogozan@users.noreply.github.com>
2022-10-17 21:15:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function AfiseazaText() {
+
+    document.querySelector("#experienta1").classList.toggle("element-ascuns");  } 
+
+
+function afiseazaText2(){
+document.querySelector("experienta2").classList.toggle("element-ascuns");
+}
